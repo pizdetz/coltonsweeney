73119a7be6bd7274d23bbd2bbafb3d687d99a6dd
Colton Sweeney <cosweene@progress.com>
2017-08-03 14:02:56 -0400

    add images and set up static files. new fonts.

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
+//set up logging variables
+var dd = new Date().getDay();
+var mm = new Date().getMonth();
+var yyyy = new Date().getFullYear();
+
+
 router.use(function (req, res, next){
-  console.log("/" + req.method);
+  var date = mm + "/" + dd + "/" + yyyy;
+  var time = new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds();
+  console.log("/" + req.method + " " + date + " " + time);
   next();
 });
 
@@ -21,6 +29,8 @@ router.get("/contact", function(req, res){
 });
 
 app.use("/", router);
+
+app.use(express.static('lib'));
 
 app.use("*", function(req, res){
   res.sendFile(path + "404.html");
